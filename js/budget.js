2e66e92c85e22fbc2cbcb26d171dc934a0962ed6
fahadMridha <sohelcse16@gmail.com>
2022-08-22 14:10:42 +0600

    player part added

---
+++ js/budget.js
@@ -0,0 +1,27 @@
+function getFieldValueById(InputId) {
+    const fieldValue = parseInt(document.getElementById(InputId).value);
+    return fieldValue;
+}
+
+function getTextElementValueById(InputId) {
+    const elementTextValue = parseInt(document.getElementById(InputId).innerText);
+    return elementTextValue;
+}
+
+function setTextElementValue(inputid, result) {
+    const elementValue = document.getElementById(inputid);
+    elementValue.innerText = result;
+}
+document.getElementById('btn-calculate').addEventListener('click', function () {
+    const perPlayerCost = getFieldValueById('player-cost');
+    const playerNumber = getTextElementValueById('player-number');
+    const totalCost = perPlayerCost * 5;
+    setTextElementValue('player-expenses', totalCost);
+})
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const playerExpenses = getTextElementValueById('player-expenses')
+    const managerCost = getFieldValueById('manager-cost');
+    const coachCost = getFieldValueById('coach-cost');
+    const totalExpenses = playerExpenses + managerCost + coachCost;
+    setTextElementValue('final-total', totalExpenses)
+})
---
+++ js/common.js
@@ -0,0 +1,5 @@
+function addToList(element) {
+    // console.log(element.parentNode.parentNode.children[1].innerText)
+    const playerName = element.parentNode.parentNode.children[1].innerText
+    console.log(playerName)
+}
